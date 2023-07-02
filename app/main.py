043841d8041d650dd1f74932f95b93ee7cf2cfdf
JoaoAugustoMV <93055242+JoaoAugustoMV@users.noreply.github.com>
2023-07-02 13:11:09 -0300

    Add CORS local

--- app/main.py
+++ app/main.py
@@ -2,6 +2,8 @@ from fastapi import Depends, FastAPI
 import uvicorn
 from routers import certificadoApovacao
 
+from fastapi.middleware.cors import CORSMiddleware
+
 tag_ca = [
     {
         "name": "Certificado de Aprovação",
@@ -16,8 +18,17 @@ app = FastAPI(
         openapi_tags=tag_ca
     )
 
+origins = [
+    "http://localhost:4200",
+]
 
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(certificadoApovacao.router)
 
 @app.get("/", tags=["HOME"])
@@ -25,4 +36,4 @@ async def index():
     return "HOME"
 
 if __name__ == "__main__":
-    uvicorn.run(app)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
