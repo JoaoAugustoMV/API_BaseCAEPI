b3aebf25b8532b3f96cc6f88b47e12f475d60d67
JoaoAugustoMV <93055242+JoaoAugustoMV@users.noreply.github.com>
2023-09-13 16:01:34 -0300

    Tratando linhas inconsistentes Tbm pequenas refatorações

--- app/Services/BaseDadosCaEPI.py
+++ app/Services/BaseDadosCaEPI.py
@@ -3,43 +3,86 @@ import io
 import zipfile
 import pandas as pd
 import os
-
+import re
 
 class BaseDadosCaEPI:
     baseDadosDF = None 
-    nomeArquivo = 'tgg_export_caepi.txt'      
+    nomeArquivoBase = 'tgg_export_caepi.txt'
+    nomeArquivoErros = 'CAs_com_erros.txt'    
+    urlBase = 'ftp.mtps.gov.br'
+    caminho = 'portal/fiscalizacao/seguranca-e-saude-no-trabalho/caepi/'
+    nColunas = 19
+
     def __init__(self):
         self = self
 
     def _baixarArquivoBaseCaEPI(self):
-        if os.path.exists(self.nomeArquivo):
-            os.remove(self.nomeArquivo)
+        if os.path.exists(self.nomeArquivoBase):
+            os.remove(self.nomeArquivoBase)
 
-        urlBase = 'ftp.mtps.gov.br'
-        caminho = 'portal/fiscalizacao/seguranca-e-saude-no-trabalho/caepi/'
-
-        ftp = ftplib.FTP(urlBase)
+        ftp = ftplib.FTP(self.urlBase)
         ftp.login()
-        ftp.cwd(caminho)
+        ftp.cwd(self.caminho)
 
-        arquivoZip = 'tgg_export_caepi.zip'
+        nomeArquivoZip = 'tgg_export_caepi.zip'
         r = io.BytesIO()
 
-        ftp.retrbinary(f'RETR {arquivoZip}', r.write)
+        ftp.retrbinary(f'RETR {nomeArquivoZip}', r.write)
 
         arquivoZip = zipfile.ZipFile(r)
 
         arquivoZip.extractall()
     
-    def _transformarEmDataFrame(self):                  
-        self.baseDadosDF = pd.read_csv(self.nomeArquivo, sep=r"(?<!\s)\|(?!\s)",engine='python', encoding='unicode_escape', quoting=3, quotechar='"', dtype="string")
+    def _transformarEmDataFrame(self):          
+        listaCas = self._retornarCAsSemErros()
+        cols = listaCas[0]
+        self.baseDadosDF = pd.DataFrame(listaCas, columns=cols)
         if('RegistroCA' not in self.baseDadosDF.columns):
-            self.baseDadosDF = self.baseDadosDF.rename(columns = {'#NRRegistroCA':'RegistroCA'})
+            self.baseDadosDF = self.baseDadosDF.rename(columns = {'#NRRegistroCA':'RegistroCA'})        
 
+    def _retornarCAsSemErros(self) -> list:
+        listaCAsValidos = []
+        listaCAsInvalidos = []
+
+        arquivo = open(self.nomeArquivoBase, encoding='unicode_escape')
+        for linha in arquivo.readlines():
+            linhaDf = linha.split('|')
+            if len(linhaDf) > self.nColunas:
+                resul_tratamento = self._tratarCasComErros(linha)
+                if resul_tratamento['sucess']:
+                    linhaDf = resul_tratamento['linha']
+                else:
+                    listaCAsInvalidos.append(linha)
+                    continue
+    
+            listaCAsValidos.append(linhaDf)
+        
+        if listaCAsInvalidos:
+            self._criarArquivoComErros(listaCAsInvalidos)
+        return listaCAsValidos
+    
+    def _tratarCasComErros(self, linha) -> dict:
+        linhaDf = re.split(r'(?<! )\|', linha)
+        if len(linhaDf) > self.nColunas: # Erro
+            return {
+                'sucess': False,
+                'linha': linha
+            }
+
+        return    {
+            'sucess': True,
+            'linha': linhaDf
+        }
+
+    def _criarArquivoComErros(self, listaCAsInvalidos:list) -> None:
+        with open(self.nomeArquivoErros, 'w') as f:
+            f.writelines(listaCAsInvalidos)
+    
     def retornarBaseDados(self) -> pd.DataFrame:
-        print("Aguarde o download...")        
-        self._baixarArquivoBaseCaEPI()
-        print(f"Download concluido!")
+        if not os.path.exists(self.nomeArquivoBase):
+            print("Aguarde o download...")        
+            self._baixarArquivoBaseCaEPI()
+            print(f"Download concluido!")
 
         self._transformarEmDataFrame()
         return self.baseDadosDF
